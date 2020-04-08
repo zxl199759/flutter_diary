586a94dc94a163394f65023647af5149ac204ea4
xxsl <1572742545@qq.com>
2020-04-09 01:29:40 +0800

    no message

---
+++ lib/diary.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/Provider/flutterSQLite.dart';
+import 'package:flutter_demo/myBehavior.dart';
+import 'package:flutter_demo/topTime.dart';
+import 'package:provider/provider.dart';
+
+class Diary extends StatefulWidget {
+  @override
+  _DiaryState createState() => _DiaryState();
+}
+
+class _DiaryState extends State<Diary> {
+  @override
+  Widget build(BuildContext context) {
+    final diaryList = Provider.of<Sqlite>(context).diaryList;
+    return SafeArea(
+      child: Container(
+        color: Color.fromRGBO(240, 240, 240, 1),
+        child: Column(
+          children: <Widget>[
+            //头部时间
+            TopTime(DateTime.now()),
+            Expanded(
+              //去除滑动蓝色波纹
+              child: ScrollConfiguration(
+                //去除原因
+                behavior: MyBehavior(),
+                child: ListView.builder(
+                  itemCount: diaryList.length,
+                  itemBuilder: (context, index) {
+                    return DiaryFactory(
+                      diaryList[index]['id'],
+                      diaryList[index]['year'].toString(),
+                      diaryList[index]['month'].toString(),
+                      diaryList[index]['day'].toString(),
+                      diaryList[index]['diary'],
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+//日记工厂
+class DiaryFactory extends StatelessWidget {
+  final int id;
+  final String year;
+  final String month;
+  final String day;
+  final String text;
+
+  DiaryFactory(this.id,this.year, this.month, this.day, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPress:(){
+        Provider.of<Sqlite>(context,listen: false).dele(id);
+      },
+      onTap: (){
+        print('0');
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width - 20,
+        margin: EdgeInsets.only(top: 10),
+        padding: EdgeInsets.fromLTRB(10, 20, 20, 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                year + '年' + month + '月' + day + '日',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black38,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                text,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/Provider/flutterSQLite.dart';
+import 'package:flutter_demo/Provider/themeColor.dart';
+
+import 'package:flutter_demo/logoAnimat.dart';
+import 'package:flutter_demo/router.dart';
+import 'package:provider/provider.dart';
+
+
+
+
+main() {
+  runApp(
+    MultiProvider(
+    providers: [
+      ChangeNotifierProvider.value(value: Sqlite()),
+      ChangeNotifierProvider.value(value: ThemeColor(0)),
+    ],
+    child:  MyApp(),
+  )
+   
+    );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<Sqlite>(context,listen: false)..findUser()..findAll();
+    Provider.of<ThemeColor>(context,listen: false).find();
+    
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: onGenerateRoute,  
+      home: Scaffold(
+        body:LogoAnimat(),
+      ),   
+    );
+  }
+}
