0293321e880b08b8dcb719e5b6e34ff0498f4bad
xxsl <1572742545@qq.com>
2020-04-09 00:59:35 +0800

    no message

---
+++ build/app/intermediates/flutter/profile/android-arm64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: F:\flutter\bin\cache\artifacts\engine\android-arm64-profile\windows-x64\gen_snapshot
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <image_picker/ImagePickerPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+}
+
+@end
